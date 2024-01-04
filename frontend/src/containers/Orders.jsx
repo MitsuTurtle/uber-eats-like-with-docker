53b6a574d5b509e0fcc86ea4a1d9328b5d2b13ab
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-01-04 11:20:25 +0900

    仮注文詳細のAPIを叩く

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,9 +1,23 @@
-import React, { Fragment } from 'react';
+import { fetchLineFoods } from 'apis/line_foods';
+import { Fragment, useEffect, useReducer } from 'react';
+// reducers
+import { initialState, lineFoodsActionTypes, lineFoodsReducer } from 'reducers/lineFood';
 
 export const Orders = () => {
-  return (
-    <Fragment>
-      注文画面
-    </Fragment>
-  )
-}
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            linFoodSummary: data,
+          },
+        })
+      )
+      .catch((e) => console.error(e));
+  }, []);
+  return <Fragment>注文画面</Fragment>;
+};
